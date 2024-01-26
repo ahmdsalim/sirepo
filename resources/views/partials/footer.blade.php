903806bcb3ac6bedc348310d6ee35237791aee1d
Ahmad Salim Asshobri <42795575+ahmdsalim@users.noreply.github.com>
2024-01-26 21:08:24 +0700

    Delete resources directory

--- resources/views/partials/footer.blade.php
+++
@@ -1,8 +0,0 @@
-<footer class="content__boxed mt-auto">
-   <div class="content__wrap py-3 py-md-1 d-flex flex-column flex-md-row align-items-md-center">
-      <div class="text-nowrap mb-4 mb-md-0">Copyright &copy; {{ date('Y') }} <a href="/" class="ms-1 btn-link fw-bold">ruangbaca.me</a></div>
-      <nav class="nav flex-column gap-1 flex-md-row gap-md-3 ms-md-auto" style="row-gap: 0 !important;">
-         <a class="nav-link px-0">Versi 1.0</a>
-      </nav>
-   </div>
-</footer>
